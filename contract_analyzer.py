75bc800c6d35b910c89648a5260ff376f4ab1780
Srikaran <151755108+Srikaran5@users.noreply.github.com>
2026-02-06 14:05:46 +0000

    Modified

--- contract_analyzer.py
+++ contract_analyzer.py
@@ -1,5 +1,11 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
+from spacy.cli import download
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 IMPORTANT_KEYWORDS = {
     "Penalty": ["penalty", "fine", "charge"],
